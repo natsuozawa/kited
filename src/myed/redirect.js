423eeb37eab129b47bc0d17aa48855af3a89cbad
natsuozawa <natsuozawa@outlook.com>
2021-07-04 00:03:11 +0900

    Fix looping bug at myed login button

--- src/myed/redirect.js
+++ src/myed/redirect.js
@@ -8,8 +8,8 @@ let interval;
 const redirect = () => {
   if (document.getElementById("root").childElementCount === 0) return;
   const login = document.getElementById("login-btn");
-  if (login) login.click();
-  else clearInterval(interval);
+  if (login) location.href = "https://www.ease.ed.ac.uk/cosign.cgi?cosign-eucsCosign-www.myed.ed.ac.uk&https://www.myed.ed.ac.uk/uPortal/Login?refUrl=%2Fmyed-progressive%2F";
+  clearInterval(interval);
 };
 
 interval = setInterval(redirect, 500);
